7405a5b4b2e9df17fdbca2b4ba7ac4535c677818
abinaya-Gopalan <68542415+abinaya-Gopalan@users.noreply.github.com>
2021-02-07 15:12:12 +0530

    Update reverseString.java

--- MasterTheCodingInterviewDataStructuresAlgorithms/reverseString.java
+++ MasterTheCodingInterviewDataStructuresAlgorithms/reverseString.java
@@ -1,1 +1,8 @@
-
+public class Solution{
+  public static void main(String args[]){
+    String str = "I like this course very much";
+    char[] charArr = str.toCharArray();
+        for (int i = charArr.length - 1; i >= 0; i--)
+            System.out.print(charArr[i]);
+  }
+}
